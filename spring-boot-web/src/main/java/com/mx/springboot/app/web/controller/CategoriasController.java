c3c8f5903d51478c0e7687ecbad961e17e6bda29
miguelpoot <93948699+miguelpoot@users.noreply.github.com>
2022-10-09 11:49:43 -0500

    xd nuevo
    hola

--- spring-boot-web/src/main/java/com/mx/springboot/app/web/controller/CategoriasController.java
+++ spring-boot-web/src/main/java/com/mx/springboot/app/web/controller/CategoriasController.java
@@ -3,6 +3,7 @@ package com.mx.springboot.app.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,6 +20,7 @@ import com.mx.springboot.app.web.service.ICategoriasService;
 public class CategoriasController {
 	
 	@Autowired
+//	@Qualifier("categoriasServiceJpa")
    	private ICategoriasService serviceCategorias;
 
 	@RequestMapping(value="/index", method=RequestMethod.GET)
